c33b6e0ef36bbc7e14ed4c256d62afa1331857a1
nur312 <nurzhigit.en@gmail.com>
2022-04-09 17:39:40 +0300

    init

---
+++ springboot/notiprice/src/main/kotlin/com/notiprice/notiprice/NotipriceApplication.kt
@@ -0,0 +1,11 @@
+package com.notiprice.notiprice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotipriceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotipriceApplication>(*args)
+}
---
+++ springboot/notiprice/src/test/kotlin/com/notiprice/notiprice/NotipriceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notiprice.notiprice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotipriceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
