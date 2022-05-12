2ba8fc772dc63228287e3d215e3301fe8b2ca420
nur312 <nurzhigit.en@gmail.com>
2022-05-12 19:06:50 +0300

    refactoring dao? login and register page

---
+++ springboot/notiprice/src/main/kotlin/com/notiprice/entity/Columns.kt
@@ -0,0 +1,25 @@
+package com.notiprice.dao
+
+class Columns {
+    companion object {
+        const val users = "users"
+        const val chatId = "chat_id"
+
+
+        const val username = "username"
+        const val password = "password"
+        const val subscriptions = "subscriptions"
+        const val productId = "product_id"
+
+
+        const val products = "products"
+        const val id = "id"
+        const val name = "name"
+        const val price = "price"
+        const val currency = "currency"
+        const val url = "url"
+        const val xpath = "xpath"
+        const val priceStr = "price_str"
+        const val lastCheck = "last_check"
+    }
+}
