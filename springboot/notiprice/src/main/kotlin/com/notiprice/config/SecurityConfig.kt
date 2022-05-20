0f0ff706ae78273cce6461fb61035218586a9fc2
nur312 <nurzhigit.en@gmail.com>
2022-05-20 12:54:39 +0300

    finish

--- springboot/notiprice/src/main/kotlin/com/notiprice/config/SecurityConfig.kt
+++ springboot/notiprice/src/main/kotlin/com/notiprice/config/SecurityConfig.kt
@@ -1,9 +1,6 @@
 package com.notiprice.config
 
 import com.notiprice.security.JwtFilter
-import com.notiprice.exception.RestTemplateResponseErrorHandler
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -12,11 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import java.time.Duration
 import java.util.*
 
 /**
@@ -25,22 +20,8 @@ import java.util.*
 @Configuration
 @EnableScheduling
 @EnableWebSecurity
-class ServiceConfig(private val jwtFilter: JwtFilter) : WebSecurityConfigurerAdapter() {
-    /**
-     * Создание экземпляра класса RestTemplate.
-     * "Instances of the JdbcTemplate class are threadsafe once configured"
-     * https://docs.spring.io/spring-framework/docs/3.0.x/spring-framework-reference/html/jdbc.html
-     */
-    @Bean
-    fun restTemplate(
-        builder: RestTemplateBuilder,
-        @Value("\${timeout.seconds.connect}") connectTimeout: Long,
-        @Value("\${timeout.seconds.read}") readTimeout: Long
-    ): RestTemplate = builder
-        .errorHandler(RestTemplateResponseErrorHandler())
-        .setConnectTimeout(Duration.ofSeconds(connectTimeout))
-        .setReadTimeout(Duration.ofSeconds(readTimeout))
-        .build()
+class SecurityConfig(private val jwtFilter: JwtFilter) : WebSecurityConfigurerAdapter() {
+
 
     /**
      * Конфигурация безопасности программы.
